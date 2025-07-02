306afc8077b3f797b8e14c1082fe394bc9d51899
Ch. Michalakis <mpampisb@gmail.com>
2025-07-02 15:05:25 +0300

    feat: added update and delete user controllers

--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -5,7 +5,9 @@ import User from "../models/user.model.ts";
 import { type Response } from "express";
 import type { RequestWithUser } from "../types/types.ts";
 
-
+/**
+ * GET CURRENT USER CONTROLLER
+ */
 export const getCurrentUser = async (req: RequestWithUser, res: Response): Promise<void> => {
   try {
     const userId = req.userId;
@@ -26,7 +28,89 @@ export const getCurrentUser = async (req: RequestWithUser, res: Response): Promi
   }
 };
 
-
+/**
+ * UPDATE USER CONTROLLER
+ */
 export const updateCurrentUser = async (req: RequestWithUser, res: Response): Promise<void> => {
+  const userId = req.userId;
+  const {
+    username, 
+    email,
+    password, 
+    firstname, 
+    lastname,
+    phoneNumber,
+  } = req.body;
 
+  try {
+    const user = await User.findById(userId).select('+password -__v').exec();
+
+    if (!user) {
+      res.status(404).json({
+        status: false, 
+        code: 'NotFound',
+        message: 'User not found',
+      })
+      return;
+    }
+
+    if (username) user.username = username;
+    if(email) user.email = email;
+    if (password) user.password = password;
+    if (firstname) user.firstname = firstname;
+    if (lastname) user.lastname = lastname;
+    if (phoneNumber) user.phoneNumber = phoneNumber;
+
+    // await user.save();
+    const updated = await User.findOneAndUpdate({ _id: userId }, user, { new: true }).lean();  // new: true -> returns updated user | .lean() -> returns json()
+
+    LOGGER.info(`User with id: ${userId} updated successfully.`, updated);
+
+    res.status(200).json({
+      updated
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      code: 'ServerError',
+      message: 'Internal Server Error',
+      error: error
+    })
+    LOGGER.error(`Failed to update user with id ${userId}. Error: `, error);
+  }
+}
+
+
+/**
+ * DELETE CURRENT USER CONTROLLER
+ */
+export const deleteCurrentUser = async (req: RequestWithUser, res: Response): Promise<void> => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      res.status(404).json({
+        status: false, 
+        code: 'NotFound',
+        message: 'User not found',
+      })
+      return;
+    }
+    await User.findOneAndDelete({_id: userId});
+
+    res.status(200).json({
+      status: true,
+      message: 'User deleted successfully.'
+    })
+  } catch(error) {
+    res.status(500).json({
+      status: false,
+      code: 'ServerError',
+      message: 'Internal Server Error',
+      error: error
+    })
+    LOGGER.error(`Failed to delete user with id ${userId}. Error: `, error);
+  }
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { param, query, body } from 'express-validator';
 
-import { getCurrentUser, updateCurrentUser } from '../controllers/user-controllers.ts';
+import { getCurrentUser, updateCurrentUser, deleteCurrentUser } from '../controllers/user-controllers.ts';
 
 import { Roles } from '../types/types.ts';
 
@@ -9,6 +9,7 @@ import { Roles } from '../types/types.ts';
 import authenticate from '../middlewares/authenticate.ts';
 import validateRequest from '../middlewares/validate-request.ts';
 import authorize from '../middlewares/authorize.ts';
+import { registerValidator } from '../validators/auth-validator.ts';
 
 import User from '../models/user.model.ts';
 
@@ -16,6 +17,8 @@ const router = Router();
 
 router.get('/current', authenticate, authorize([Roles.Admin, Roles.User]), getCurrentUser);
 
-router.put('/current', authenticate, authorize([Roles.Admin, Roles.User]), updateCurrentUser);
+router.put('/current', authenticate, authorize([Roles.Admin, Roles.User]), updateCurrentUser);  // TODO: Add Validation In Fields To Update
+
+router.delete('/current', authenticate, authorize([Roles.Admin, Roles.User]), deleteCurrentUser)
 
 export default router;
